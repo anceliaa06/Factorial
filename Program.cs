1acf2918662fd3b4b7fee709507f3efa8243f1c8
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-16 23:22:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("Factorial of " + num + " is: " + factorial);
+        }
+    }
+}
